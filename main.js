3399556d20d4ca60e0af9dbefcde03e0fba4d770
Michael Currin <michael.currin@gmail.com>
2020-01-31 22:12:12 +0200

    feat: Remove empty lines

--- main.js
+++ main.js
@@ -14,56 +14,59 @@ function makeBadge(title, imgUrl, extUrl) {
     return `[![${title}](${imgUrl})](${extUrl})`;
 }
 
-function getInputs() {
-    var useThisTemplate = $('input[name="use-this-template"').prop('checked'),
+function useThisTemplateBadge(show, username, repoName) {
+    if (show && username && repoName) {
+        var text = 'Use_this_template',
+            color = 'green';
+
+        var title = 'Use this template',
+            imgUrl = `https://img.shields.io/badge/${text}-${color}.svg`,
+            extUrl = `https://github.com/${username}/${repoName}/generate`;
+
+        return makeBadge(title, imgUrl, extUrl);
+    }
+    return null;
+}
+
+function tagBadge(username, repoName) {
+    if (username && repoName) {
+        var title = 'GitHub tag',
+            imgUrl = `https://img.shields.io/github/tag/${username}/${repoName}.svg`,
+            extUrl = `https://GitHub.com/${username}/${repoName}/tags/`;
+
+        return makeBadge(title, imgUrl, extUrl);
+    }
+    return '';
+}
+
+function licenseBadge(licenseType, username, repoName) {
+    if ((licenseType, username, repoName)) {
+        var color = 'blue',
+            branch = 'master';
+
+        var title = `${licenseType} license`,
+            imgUrl = `https://img.shields.io/badge/License-${licenseType}-${color}.svg`,
+            extUrl = `https://github.com/${username}/${repoName}/blob/${branch}/LICENSE`;
+
+        return makeBadge(title, imgUrl, extUrl);
+    }
+    return '';
+}
+
+function makeBadges() {
+    var useThisTemplateIsChecked = $('input[name="use-this-template"').prop('checked'),
         username = $('input[name="username"').val(),
         repoName = $('input[name="repo-name"').val(),
         licenseType = $('input[name="license-type"').val();
 
     return {
-        USE_THIS_TEMPLATE: useThisTemplate,
-        USERNAME: username,
-        REPO_NAME: repoName,
-        LICENSE_TYPE: licenseType,
-        use: function() {
-            if (useThisTemplate && this.USERNAME && this.REPO_NAME) {
-                var text = 'Use_this_template',
-                    color = 'green';
-
-                var title = 'Use this template',
-                    imgUrl = `https://img.shields.io/badge/${text}-${color}.svg`,
-                    extUrl = `https://github.com/${this.USERNAME}/${this.REPO_NAME}/generate`;
-
-                return makeBadge(title, imgUrl, extUrl);
-            }
-            return '';
-        },
-        tag: function() {
-            if (this.USERNAME && this.REPO_NAME) {
-                var title = 'GitHub tag',
-                    imgUrl = `https://img.shields.io/github/tag/${this.USERNAME}/${this.REPO_NAME}.svg`,
-                    extUrl = `https://GitHub.com/${this.USERNAME}/${this.REPO_NAME}/tags/`;
-
-                return makeBadge(title, imgUrl, extUrl);
-            }
-            return '';
-        },
-        license: function() {
-            if (this.LICENSE_TYPE && this.USERNAME && this.REPO_NAME) {
-                var color = 'blue',
-                    branch = 'master';
-
-                var title = `${this.LICENSE_TYPE} license`,
-                    imgUrl = `https://img.shields.io/badge/License-${this.LICENSE_TYPE}-${color}.svg`,
-                    extUrl = `https://github.com/${this.USERNAME}/${this.REPO_NAME}/blob/${branch}/LICENSE`;
-
-                return makeBadge(title, imgUrl, extUrl);
-            }
-            return '';
-        }
+        use: useThisTemplateBadge(useThisTemplateIsChecked, username, repoName),
+        tag: tagBadge(username, repoName),
+        license: licenseBadge(licenseType, username, repoName)
     };
 }
 
+/** Convert markdown to HTML. */
 function mdToHtml(markdown) {
     var reader = new commonmark.Parser(),
         writer = new commonmark.HtmlRenderer(),
@@ -72,18 +75,19 @@ function mdToHtml(markdown) {
     return writer.render(parsed);
 }
 
+/** Place HTML inside target template, but respecting markdown. **/
 function renderTemplate(templateID, outputId, data) {
     var template = $(templateID).html(),
-        resultHtml = Mustache.to_html(template, data),
-        resultMd = mdToHtml(resultHtml);
+        // Remove empty lines of result.
+        resultHtml = Mustache.to_html(template, data).replace(/^\n/gm, '');
 
+    resultMd = mdToHtml(resultHtml);
     $(outputId).html(resultMd);
 }
 
 function renderAll() {
-    var data = getInputs();
-    console.debug(data);
+    var badgeData = makeBadges();
 
-    renderTemplate('#badges', '#target-output', data);
-    renderTemplate('#badges-md', '#target-output-md', data);
+    renderTemplate('#badges', '#target-output', badgeData);
+    renderTemplate('#badges-md', '#target-output-md', badgeData);
 }
