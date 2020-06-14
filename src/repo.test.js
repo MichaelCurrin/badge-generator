1ae17df36782d9a5d9700ac8b84ba715378448fa
Michael Currin <michael.currin@gmail.com>
2020-06-14 10:50:04 +0200

    test: Create repo and license tests

---
+++ src/repo.test.js
@@ -0,0 +1,13 @@
+const { Repo } = require('./repo.js')
+
+test('Get correct full URL for a repo', () => {
+  var repo = new Repo('my-user', 'my-repo-name')
+
+  expect(repo.url()).toBe('https://github.com/my-user/my-repo-name')
+})
+
+test('Get correct full license URL for a repo', () => {
+  var repo = new Repo('my-user', 'my-repo-name')
+
+  expect(repo.licenseUrl()).toBe('https://github.com/my-user/my-repo-name/blob/master/LICENSE')
+})
