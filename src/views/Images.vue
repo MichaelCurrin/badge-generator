35ae70d3fe90233d5b52176e24ce2f69068558c4
Michael Currin <michael.currin@gmail.com>
2020-09-13 19:00:38 +0200

    refactor: Create and use component

--- src/views/Images.vue
+++ src/views/Images.vue
@@ -11,7 +11,7 @@
         <h2>Input values</h2>
 
         <form @submit.prevent.enter="submit">
-          <fieldset name="image">
+          <fieldset name="images">
             <legend>Documentation images</legend>
 
             <div>
@@ -84,6 +84,7 @@
         </form>
       </div>
 
+      <!-- TODO make component -->
       <div class="app-notes">
         <h2>Help</h2>
 
@@ -115,31 +116,13 @@
     </div>
 
     <div class="col-6">
-      <!-- TODO turn into component -->
-      <div class="app-results">
-        <h2>Output values</h2>
-
-        <div>
-          <h3>Preview</h3>
-          <!-- Make sure to not add result on a new line otherwise it will indent and format. -->
-          <!-- Make sure to use :source since {{ result }} as inner text will fail to update on UI changes. -->
-          <vue-markdown :source="result"></vue-markdown>
-        </div>
-
-        <hr />
-
-        <div>
-          <h3>Code</h3>
-          <!-- To reduce whitespace, make sure to keep everything on one line here. -->
-          <pre><code>{{ result }}</code></pre>
-        </div>
-      </div>
+      <AppResults :result="result" />
     </div>
   </div>
 </template>
 
 <script>
-import VueMarkdown from "vue-markdown";
+import AppResults from "@/components/AppResults.vue";
 
 import { markdownImageWithLink } from "../badges";
 import { mkHtmlImg } from "../images";
@@ -148,7 +131,7 @@ import { stripLeadingSlash } from "../lib";
 export default {
   name: "Images",
   components: {
-    VueMarkdown,
+    AppResults,
   },
   data() {
     return {
