5c04b6d7bb65faad193c897d1bc2d3f57f009328
Michael Currin <michael.currin@gmail.com>
2020-09-13 15:45:38 +0200

    style: Update indentation

--- src/images.js
+++ src/images.js
@@ -4,24 +4,24 @@
 
 // TODO There must be cleaner way to calculate this and lay it out. Use Vue?
 export function mkHtmlImg(src, alt, title, target = '', width, height, alignAttr) {
-    var htmlTitle = title ? ` title="${title}"` : '',
-        htmlAltText = alt ? ` alt="${alt}"` : '',
-        htmlWidth = width ? ` width="${width}"` : '',
-        htmlHeight = height ? ` height="${height}"` : '';
+  var htmlTitle = title ? ` title="${title}"` : '',
+    htmlAltText = alt ? ` alt="${alt}"` : '',
+    htmlWidth = width ? ` width="${width}"` : '',
+    htmlHeight = height ? ` height="${height}"` : '';
 
-    var htmlImg = `<img src="${src}"${htmlAltText}${htmlTitle}${htmlWidth}${htmlHeight} />`;
-    if (target) {
-        htmlImg = `\
+  var htmlImg = `<img src="${src}"${htmlAltText}${htmlTitle}${htmlWidth}${htmlHeight} />`;
+  if (target) {
+    htmlImg = `\
 <a href="${target}">
     ${htmlImg}
 </a>`;
-    }
+  }
 
-    if (alignAttr) {
-        htmlImg = `\
+  if (alignAttr) {
+    htmlImg = `\
 <div align="${alignAttr}">
 ${htmlImg}
 </div>`;
-    }
-    return htmlImg;
+  }
+  return htmlImg;
 }
