1c73410db56ca0c3588b0ed4c166629f8fdac941
Michael Currin <michael.currin@gmail.com>
2020-09-14 10:12:16 +0200

    feat: Update component

--- src/components/TextInput.vue
+++ src/components/TextInput.vue
@@ -1,8 +1,9 @@
 // TODO: Fix this so it can pass a value back to the parent. See also label and name and for other fields on W3 schools. And type="text" but that seems left off. See also docs. https://vuejs.org/v2/guide/forms.html
+// https://dev.to/jakzaizzat/avoid-mutating-a-prop-directly-ab9
 <template>
   <div>
-    <span>{{ label }}: {{ bindVar }}</span>
-    <input v-model="bindVar" />
+    <span>{{ label }}: {{ bindVar }} {{ bindVarComponent }}</span>
+    <input v-bind:bindVar="bindVar" />
     <small v-if="note">{{ note }}</small>
   </div>
 </template>
@@ -11,13 +12,15 @@
 export default {
   name: "TextInput",
   props: {
-    label: String,
-    note: String,
+    label: { type: String, required: true },
+    bindVar: { type: String, required: true },
+    note: { type: String, required: false },
   },
-  data() {
-    return {
-      bindVar: "",
-    };
+  computed: {
+    // Avoid error on mutating property of parent by using the flow.
+    bindVarComponent() {
+      return this.bindVar;
+    },
   },
 };
 </script>
