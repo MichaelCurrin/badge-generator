b7ae116c1d1bf094a8a08dd659b15a66794d3692
Michael Currin <michael.currin@gmail.com>
2020-09-16 21:17:46 +0200

    feat: Add type and placeholder to TextInput

--- src/components/TextInput.vue
+++ src/components/TextInput.vue
@@ -1,10 +1,13 @@
 // Allow a given variable from the parent to be used here and push changes back up using events.
+// Quickstart: <TextInput label="" v-model="" placeholder="" note=""/>
 <template>
   <div>
     <span>{{ label }}: </span>
     <input
+      type="text"
       v-bind:value="value"
       v-on:input="$emit('input', $event.target.value)"
+      v-bind:placeholder="placeholder"
     />
     <small v-if="note">
       <vue-markdown>{{ note }}</vue-markdown>
@@ -23,6 +26,7 @@ export default {
   props: {
     label: { type: String, required: true },
     value: { type: String, required: true },
+    placeholder: { type: String, required: false },
     note: { type: String, required: false },
   },
 };
