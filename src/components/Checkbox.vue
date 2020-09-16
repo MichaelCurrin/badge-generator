81a1fef0a24b293e287cf4b4cea2b3b28b9efcf6
Michael Currin <michael.currin@gmail.com>
2020-09-16 10:02:56 +0200

    feat: Create Checkbox.vue

---
+++ src/components/Checkbox.vue
@@ -0,0 +1,32 @@
+// Bind to given variable in parent's v-model.
+// Optionally supply `:check=true` to override default of `false`.
+<template>
+  <div>
+    <span>{{ label }}: </span>
+    <input
+      type="checkbox"
+      checked="checked"
+      v-bind:value="value"
+      v-on:change="$emit('input', $event.target.checked)"
+    />
+    <small v-if="note">
+      <vue-markdown>{{ note }}</vue-markdown>
+    </small>
+  </div>
+</template>
+
+<script>
+import VueMarkdown from "vue-markdown";
+
+export default {
+  name: "Checkbox",
+  components: {
+    VueMarkdown,
+  },
+  props: {
+    label: { type: String, required: true },
+    checked: { type: Boolean, required: false, default: false },
+    note: { type: String, required: false },
+  },
+};
+</script>
