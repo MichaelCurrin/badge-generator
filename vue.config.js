33be82ec818e46170b0701a64afcde2586be5518
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2020-09-20 16:25:33 +0200

    style: Apply lint fix

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/badge-generator/'
-    : '/'
-}
+  publicPath: process.env.NODE_ENV === "production" ? "/badge-generator/" : "/",
+};
