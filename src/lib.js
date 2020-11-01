d34b44e52f7fa4b55e2cf29e74bea058415a98cd
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2020-11-01 11:30:28 +0200

    feat: Add slugify

--- src/lib.js
+++ src/lib.js
@@ -5,3 +5,12 @@
 export function stripLeadingSlash(value) {
   return value.replace(/^\/+/, "");
 }
+
+export function slugify(value) {
+  return value
+    .replace(/^\s+|\s+$/g, "") // Trim
+    .toLowerCase()
+    .replace(/[^a-z0-9 -]/g, "")
+    .replace(/\s+/g, "-") // Collapse whitespace
+    .replace(/-+/g, "-"); // Collapse dashes
+}
