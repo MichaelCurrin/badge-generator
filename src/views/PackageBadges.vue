a3144f391429a4673eb3286922f881b7d1625b94
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2020-11-04 10:28:04 +0200

    feat: Add badge

--- src/views/PackageBadges.vue
+++ src/views/PackageBadges.vue
@@ -57,6 +57,33 @@
             </fieldset>
             <br />
 
+            <fieldset name="ghRepo">
+              <legend>GitHub repo metadata</legend>
+
+              <TextInput label="Username" v-model="username" />
+              <br />
+
+              <TextInput label="Repo name" v-model="repoName" />
+            </fieldset>
+            <br />
+
+            <fieldset name="appearance">
+              <legend>Appearance</legend>
+              <TextInput
+                label="Logo"
+                v-model="logo"
+                placeholder="e.g. vue.js"
+                note="Supports values like `dependabot`, `discord` and `npm`. And hundreds of logos from [SimpleIcons](https://simpleicons.org/) such as `python`, `node.js` and `visual-studio-code`"
+              />
+
+              <TextInput
+                label="Logo color"
+                v-model="logoColor"
+                note="The badge's own color will be used unless you specify an override. Using `white` is great for readability against the dark label background."
+              />
+            </fieldset>
+            <br />
+
             <input class="btn" type="submit" value="Submit" />
           </form>
         </div>
@@ -66,26 +93,44 @@
         <Results :result="result" />
       </div>
     </div>
+
+    <div class="row">
+      <div class="col-12">
+        <Help :message="note" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Help from "@/components/Help.vue";
 import Results from "@/components/Results.vue";
 import TextInput from "@/components/TextInput.vue";
 
-import { Package } from "@/core/Package";
+import { packageVersion, Package } from "@/core/Package";
+
+const note = `
+Optionally set Repo fields to your _own_ project, so the badge dynamically pick up version number of the package chosen above.
+`;
 
 export default {
   name: "PackageBadges",
   components: {
+    Help,
     Results,
     TextInput,
   },
   data() {
     return {
-      pkgName: "requests",
-      pkgType: "python",
+      pkgName: "vue",
+      pkgType: "node",
+      username: "MichaelCurrin",
+      repoName: "badge-generator",
+      logo: "vue.js",
+      logoColor: "white",
+
       result: "_Your output will appear here_",
+      note: note,
     };
   },
   methods: {
@@ -95,8 +140,19 @@ export default {
       const pkg = new Package(this.pkgName, this.pkgType),
         badge = pkg.badge();
 
+      const lockedPkgBadge = packageVersion(
+        this.username,
+        this.repoName,
+        this.pkgName,
+        this.pkgType,
+        this.logo,
+        this.logoColor
+      );
+
       this.result = `\
 ${badge}
+
+${lockedPkgBadge}
         `;
     },
   },
