ed8aefd10d362f710f4a89606b7bfa55717e89c2
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2020-11-29 22:15:13 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,7 @@
-import Vue from "vue";
+import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router";
+import router from "./router/index";
 
-Vue.config.productionTip = false;
-
-new Vue({
-  router,
-  render: (h) => h(App),
-}).$mount("#app");
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
