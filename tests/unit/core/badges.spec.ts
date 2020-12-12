af67d71a9abc18d2e4c8bcf5a9faf6a86e31755e
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2020-12-12 19:09:43 +0200

    test: Update badges.spec.ts

--- tests/unit/core/badges.spec.ts
+++ tests/unit/core/badges.spec.ts
@@ -2,9 +2,9 @@ import { markdownImage, markdownLink } from "@/core/badges";
 
 describe("#markdownLink", () => {
   it("returns a valid markdown link", () => {
-    expect(markdownLink("Alt text", "foo.md")).toMatch("[Alt text](foo.md)");
+    expect(markdownLink("Alt text", "foo.md")).toBe("[Alt text](foo.md)");
 
-    expect(markdownLink("Example", "https://example.com")).toMatch(
+    expect(markdownLink("Example", "https://example.com")).toBe(
       "[Example](https://example.com)"
     );
   });
@@ -12,20 +12,20 @@ describe("#markdownLink", () => {
 
 describe("#markdownImage", () => {
   it("returns a valid markdown image", () => {
-    expect(markdownImage("Alt text", "foo.md")).toEqual("![Alt text](foo.md)");
+    expect(markdownImage("Alt text", "foo.md")).toBe("![Alt text](foo.md)");
 
-    expect(markdownImage("Example", "https://example.com")).toEqual(
+    expect(markdownImage("Example", "https://example.com")).toBe(
       "![Example](https://example.com)"
     );
   });
 
   it("returns a valid markdown image with hover text", () => {
-    expect(markdownImage("Alt text", "foo.md", "My hover text")).toEqual(
+    expect(markdownImage("Alt text", "foo.md", "My hover text")).toBe(
       '![Alt text](foo.md "My hover text")'
     );
 
     expect(
       markdownImage("Example", "https://example.com", "Go to example")
-    ).toEqual('![Example](https://example.com "Go to example")');
+    ).toBe('![Example](https://example.com "Go to example")');
   });
 });
