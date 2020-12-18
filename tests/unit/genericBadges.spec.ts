23215088887aa64317f7285d35a5607d49696f3a
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2020-12-18 18:46:43 +0200

    refactor: Move tests to new file

---
+++ tests/unit/genericBadges.spec.ts
@@ -0,0 +1,73 @@
+/**
+ * Generic badges test module.
+ *
+ * Tes that the UI defaults and common choices associated with the Generic view are working.
+ */
+import { genericBadge } from "@/core/badges";
+
+describe("#genericBadge", () => {
+  it("displays a badge given a message and a color", () => {
+    expect(genericBadge("", "Bar", "green")).toBe(
+      "![Bar](https://img.shields.io/badge/Bar-green)"
+    );
+  });
+
+  it("displays a badge given label, message and a color", () => {
+    expect(genericBadge("Foo", "Bar", "green")).toBe(
+      "![Foo - Bar](https://img.shields.io/badge/Foo-Bar-green)"
+    );
+  });
+
+  it("displays a badge point to external link", () => {
+    const target = "https://example.com";
+
+    expect(genericBadge("", "Bar", "green", false, target)).toBe(
+      "[![Bar](https://img.shields.io/badge/Bar-green)](https://example.com)"
+    );
+
+    expect(genericBadge("", "Bar", "green", false, target, "", "", true)).toBe(
+      "[![Bar](https://img.shields.io/static/v1?message=Bar&color=green)](https://example.com)"
+    );
+  });
+
+  it("displays a large badge", () => {
+    const displayLarge = true;
+
+    expect(genericBadge("", "Bar", "green", displayLarge)).toBe(
+      "![Bar](https://img.shields.io/badge/Bar-green?style=for-the-badge)"
+    );
+
+    expect(
+      genericBadge("", "Bar", "green", displayLarge, "", "", "", true)
+    ).toBe(
+      "![Bar](https://img.shields.io/static/v1?message=Bar&color=green&style=for-the-badge)"
+    );
+  });
+
+  it("displays a logo", () => {
+    const logo = "github";
+
+    expect(genericBadge("", "Bar", "green", false, "", logo)).toBe(
+      "![Bar](https://img.shields.io/badge/Bar-green?logo=github)"
+    );
+
+    expect(genericBadge("", "Bar", "green", false, "", logo, "", true)).toBe(
+      "![Bar](https://img.shields.io/static/v1?message=Bar&color=green&logo=github)"
+    );
+  });
+
+  it("displays a logo with a custom logo color", () => {
+    const logo = "github",
+      logoColor = "yellow";
+
+    expect(genericBadge("", "Bar", "green", false, "", logo, logoColor)).toBe(
+      "![Bar](https://img.shields.io/badge/Bar-green?logo=github&logoColor=yellow)"
+    );
+
+    expect(
+      genericBadge("", "Bar", "green", false, "", logo, logoColor, true)
+    ).toBe(
+      "![Bar](https://img.shields.io/static/v1?message=Bar&color=green&logo=github&logoColor=yellow)"
+    );
+  });
+});
