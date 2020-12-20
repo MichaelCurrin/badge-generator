a80f0e8c762a8092b908ce795861bf0231919cd3
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2020-12-20 11:14:31 +0200

    refactor: Update catalogue

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -32,3 +32,103 @@ export enum REGISTRY {
   Node = "https://www.npmjs.com/package",
   Ruby = "https://rubygems.org/gems",
 }
+
+export const CATALOGUE_BADGES = [
+  {
+    label: "view",
+    message: "Documentation",
+    target: "/docs/",
+  },
+  {
+    label: "OS",
+    message: "Linux",
+    target: "https://www.linux.org/",
+    logo: "linux",
+  },
+  {
+    label: "OS",
+    message: "macOS",
+    target: "https://www.apple.com/macos/",
+    logo: "apple",
+  },
+  {
+    label: "OS",
+    message: "Windows",
+    target: "https://www.microsoft.com/",
+    logo: "windows",
+  },
+  // Netlify badge is best created on Netlify itself and not here.
+  {
+    label: "Hosted with",
+    message: "GitHub Pages",
+    target: "https://pages.github.com/",
+    logo: "github",
+  },
+  // Use an actual GitHub Actions workflow badge if you prefer. But this could be useful it is Made for GitHub Actions.
+  {
+    label: "CI",
+    message: "GitHub Actions",
+    target: "https://github.com/features/actions",
+    logo: "github-actions",
+  },
+  {
+    label: "Made with",
+    message: "Docker",
+    target: "https://www.docker.com/",
+    logo: "docker",
+  },
+  {
+    label: "Bash",
+    message: ">=3",
+    target: "https://www.gnu.org/software/bash/",
+    logo: "gnu-bash",
+  },
+  {
+    label: "Python",
+    message: ">=3.6",
+    target: "https://python.org",
+    logo: "python",
+  },
+  {
+    label: "Node.js",
+    message: ">=12",
+    target: "https://nodejs.org",
+    logo: "node.js",
+  },
+  {
+    label: "Yarn",
+    message: "1",
+    target: "https://classic.yarnpkg.com",
+    logo: "yarn",
+  },
+  {
+    label: "TypeScript",
+    message: "4",
+    target: "https://typescriptlang.org",
+    logo: "typescript",
+  },
+  {
+    label: "React",
+    message: "17",
+    target: "https://www.npmjs.com/package/react",
+    logo: "react",
+  },
+  {
+    label: "Vue",
+    message: "3",
+    target: "https://www.npmjs.com/package/vue",
+    logo: "vue.js",
+  },
+  {
+    label: "Jekyll",
+    message: "3.9",
+    target: "https://jekyllrb.com",
+    logo: "jekyll",
+  },
+  {
+    label: "Ruby",
+    message: ">=2.6",
+    target: "https://ruby-lang.org",
+    logo: "ruby",
+  },
+];
--- src/views/Catalogue.vue
+++ src/views/Catalogue.vue
@@ -18,7 +18,7 @@
 
     <div class="row">
       <div class="col-12">
-        <div v-for="item in badges" v-bind:key="item">
+        <div v-for="item in renderedBadges" v-bind:key="item">
           <vue-markdown>{{ item }}</vue-markdown>
           <pre><code>{{ item }}</code></pre>
           <br />
@@ -33,118 +33,7 @@ import Vue from "vue";
 import VueMarkdown from "vue-markdown";
 
 import { genericBadge } from "@/core/badges";
-
-const tools = [
-  // Not a tool, but useful here anyway.
-  {
-    label: "view",
-    message: "Documentation",
-    target: "/docs/",
-  },
-  {
-    label: "OS",
-    message: "Linux",
-    target: "https://www.linux.org/",
-    logo: "linux",
-  },
-  {
-    label: "OS",
-    message: "macOS",
-    target: "https://www.apple.com/macos/",
-    logo: "apple",
-  },
-  {
-    label: "OS",
-    message: "Windows",
-    target: "https://www.microsoft.com/",
-    logo: "windows",
-  },
-  // Netlify badge is best created on Netlify itself and not here.
-  {
-    label: "Hosted with",
-    message: "GitHub Pages",
-    target: "https://pages.github.com/",
-    logo: "github",
-  },
-  // Use an actual GitHub Actions workflow badge if you prefer. But this could be useful it is Made for GitHub Actions.
-  {
-    label: "CI",
-    message: "GitHub Actions",
-    target: "https://github.com/features/actions",
-    logo: "github-actions",
-  },
-  {
-    label: "Made with",
-    message: "Docker",
-    target: "https://www.docker.com/",
-    logo: "docker",
-  },
-  {
-    label: "Bash",
-    message: ">=3",
-    target: "https://www.gnu.org/software/bash/",
-    logo: "gnu-bash",
-  },
-  {
-    label: "Python",
-    message: ">=3.6",
-    target: "https://python.org",
-    logo: "python",
-  },
-  {
-    label: "Node.js",
-    message: ">=12",
-    target: "https://nodejs.org",
-    logo: "node.js",
-  },
-  {
-    label: "Yarn",
-    message: "1",
-    target: "https://classic.yarnpkg.com",
-    logo: "yarn",
-  },
-  {
-    label: "TypeScript",
-    message: "4",
-    target: "https://typescriptlang.org",
-    logo: "typescript",
-  },
-  {
-    label: "React",
-    message: "17",
-    target: "https://www.npmjs.com/package/react",
-    logo: "react",
-  },
-  {
-    label: "Vue",
-    message: "3",
-    target: "https://www.npmjs.com/package/vue",
-    logo: "vue.js",
-  },
-  {
-    label: "Jekyll",
-    message: "3.9",
-    target: "https://jekyllrb.com",
-    logo: "jekyll",
-  },
-  {
-    label: "Ruby",
-    message: ">=2.6",
-    target: "https://ruby-lang.org",
-    logo: "ruby",
-  },
-];
-const renderedTools = tools.map((tool) =>
-  genericBadge(
-    tool.label,
-    tool.message,
-    "",
-    false,
-    tool.target,
-    tool.logo || "",
-    tool.logo ? "white" : ""
-  )
-);
+import { CATALOGUE_BADGES, DEFAULT_COLOR } from "@/core/constants";
 
 export default Vue.extend({
   name: "Catalogue",
@@ -152,8 +41,20 @@ export default Vue.extend({
     VueMarkdown,
   },
   data() {
+    const renderedBadges = CATALOGUE_BADGES.map((badge) =>
+      genericBadge(
+        badge.label,
+        badge.message,
+        DEFAULT_COLOR,
+        false,
+        badge.target,
+        badge.logo || "",
+        badge.logo ? "white" : ""
+      )
+    );
+
     return {
-      badges: renderedTools,
+      renderedBadges,
     };
   },
 });
