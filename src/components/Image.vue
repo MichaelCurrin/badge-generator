feea3698c20b4a7679d115a80e26513a224c0f31
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2020-12-20 19:05:03 +0200

    feat: Create Image.vue

---
+++ src/components/Image.vue
@@ -0,0 +1,23 @@
+<template>
+  <div v-if="linkTarget">
+    <a :href="linkTarget">
+      <img :src="imgSrc" :alt="altText" />
+    </a>
+  </div>
+  <div v-else>
+    <img :src="imgSrc" :alt="altText" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "ImageComponent",
+  props: {
+    imgSrc: { type: String, required: true },
+    altText: { type: String, required: true },
+    linkTarget: { type: String, required: false },
+  },
+});
+</script>
