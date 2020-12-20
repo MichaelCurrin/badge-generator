ab3d4abae04dc26cbe23bb3e860433b2daea7aff
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2020-12-20 11:58:21 +0200

    refactor: Move constant to catalogue.ts

--- src/core/catalogue.ts
+++ src/core/catalogue.ts
@@ -1,15 +1,112 @@
 import { genericBadge } from "@/core/badges";
-import {
-  CATALOGUE_BADGES,
-  DEFAULT_COLOR,
-  DEFAULT_LOGO_COLOR,
-} from "@/core/constants";
+import { DEFAULT_COLOR, DEFAULT_LOGO_COLOR } from "@/core/constants";
 
 const DEFAULT_IS_LARGE = false;
+const BADGE_DETAILS = [
+  {
+    label: "view",
+    message: "Documentation",
+    target: "/docs/",
+    logo: "",
+    isLarge: true,
+  },
+  {
+    label: "OS",
+    message: "Linux",
+    target: "https://www.linux.org/",
+    logo: "linux",
+  },
+  {
+    label: "OS",
+    message: "macOS",
+    target: "https://www.apple.com/macos/",
+    logo: "apple",
+  },
+  {
+    label: "OS",
+    message: "Windows",
+    target: "https://www.microsoft.com/",
+    logo: "windows",
+  },
+  // Netlify badge is best created on Netlify itself and not here.
+  {
+    label: "Hosted with",
+    message: "GitHub Pages",
+    target: "https://pages.github.com/",
+    logo: "github",
+  },
+  // Use an actual GitHub Actions workflow badge if you prefer. But this could be useful it is Made for GitHub Actions.
+  {
+    label: "CI",
+    message: "GitHub Actions",
+    target: "https://github.com/features/actions",
+    logo: "github-actions",
+  },
+  {
+    label: "Made with",
+    message: "Docker",
+    target: "https://www.docker.com/",
+    logo: "docker",
+  },
+  {
+    label: "Bash",
+    message: ">=3",
+    target: "https://www.gnu.org/software/bash/",
+    logo: "gnu-bash",
+  },
+  {
+    label: "Python",
+    message: ">=3.6",
+    target: "https://python.org",
+    logo: "python",
+  },
+  {
+    label: "Node.js",
+    message: ">=12",
+    target: "https://nodejs.org",
+    logo: "node.js",
+  },
+  {
+    label: "Yarn",
+    message: "1",
+    target: "https://classic.yarnpkg.com",
+    logo: "yarn",
+  },
+  {
+    label: "TypeScript",
+    message: "4",
+    target: "https://typescriptlang.org",
+    logo: "typescript",
+  },
+  {
+    label: "React",
+    message: "17",
+    target: "https://www.npmjs.com/package/react",
+    logo: "react",
+  },
+  {
+    label: "Vue",
+    message: "3",
+    target: "https://www.npmjs.com/package/vue",
+    logo: "vue.js",
+  },
+  {
+    label: "Jekyll",
+    message: "3.9",
+    target: "https://jekyllrb.com",
+    logo: "jekyll",
+  },
+  {
+    label: "Ruby",
+    message: ">=2.6",
+    target: "https://ruby-lang.org",
+    logo: "ruby",
+  },
+];
 
 // Added here as I couldn't add a named export in the Catalogue.vue file.
 export function renderBadges() {
-  return CATALOGUE_BADGES.map((badge) =>
+  return BADGE_DETAILS.map((badge) =>
     genericBadge(
       badge.label,
       badge.message,
