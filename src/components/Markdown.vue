1bb9719fe5ef0a33ab31c05fa8593ddd175b29c6
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2020-12-28 21:59:29 +0200

    feat: Create Markdown.vue

---
+++ src/components/Markdown.vue
@@ -0,0 +1,23 @@
+// Based on VueMarkdown but much lighter - fewer features and dependencies.
+<template>
+  <div v-html="render(content)"></div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+import markdownIt from "markdown-it";
+
+// TODO The docstring says rendering from HTML is unsafe and recommends sanitizing, or using a plugin.
+const md = new markdownIt({ html: true });
+
+export default Vue.extend({
+  name: "Markdown",
+  props: {
+    content: String,
+  },
+  methods: {
+    render: (value: string) => md.render(value),
+  },
+});
+</script>
