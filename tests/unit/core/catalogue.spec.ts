f30b5915c9ff76bb91de2ac2ad460086afc02763
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2020-12-29 15:46:46 +0200

    test: Add to catalogue.spec.ts

--- tests/unit/core/catalogue.spec.ts
+++ tests/unit/core/catalogue.spec.ts
@@ -4,8 +4,13 @@ describe("#renderBadges", () => {
   it("converts configured catalogue badges to expected markdown badges", () => {
     const badges = renderBadges();
 
-    expect(badges[0]).toEqual(
+    expect(badges[0]).toBe(
       "[![view - Documentation](https://img.shields.io/badge/view-Documentation-blue?style=for-the-badge)](/docs/)"
     );
+
+    expect(badges[1]).toBe(
+      "[![OS - Linux](https://img.shields.io/badge/OS-Linux-blue?logo=linux&logoColor=white)](https://www.linux.org/)"
+    );
+
   });
 });
