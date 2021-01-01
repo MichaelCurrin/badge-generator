82b075540787599409a2200cd065fdad1bd93c57
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-01-01 11:34:28 +0200

    refactor: Update repo handling

--- tests/unit/core/Repo.spec.ts
+++ tests/unit/core/Repo.spec.ts
@@ -1,11 +1,12 @@
 import { Repo } from "@/core/Repo";
 
 describe("#Repo", () => {
-  describe("#ghURL", () => {
-    const repo = new Repo("MichaelCurrin", "badge-generator");
+  const repoNoLicense = new Repo("MichaelCurrin", "badge-generator");
+  const repoWithLicense = new Repo("MichaelCurrin", "badge-generator", "MIT");
 
+  describe("#ghURL", () => {
     it("returns a valid GitHub repo URL", () => {
-      expect(repo.ghURL()).toBe(
+      expect(repoNoLicense.ghURL()).toBe(
         "https://github.com/MichaelCurrin/badge-generator"
       );
     });
@@ -13,55 +14,52 @@ describe("#Repo", () => {
 
   describe("#ghPagesURL", () => {
     it("returns a valid GitHub Pages project URL", () => {
-      const repo = new Repo("MichaelCurrin", "badge-generator");
+      const repoNoLicense = new Repo("MichaelCurrin", "badge-generator");
 
-      expect(repo.ghPagesURL()).toBe(
+      expect(repoNoLicense.ghPagesURL()).toBe(
         "https://michaelcurrin.github.io/badge-generator/"
       );
     });
 
     it("returns a valid GitHub Pages user URL", () => {
-      const repo = new Repo("MichaelCurrin", "MichaelCurrin.github.io");
+      const repoNoLicense = new Repo(
+        "MichaelCurrin",
+        "MichaelCurrin.github.io"
+      );
 
-      expect(repo.ghPagesURL()).toBe("https://michaelcurrin.github.io");
+      expect(repoNoLicense.ghPagesURL()).toBe(
+        "https://michaelcurrin.github.io"
+      );
     });
   });
 
   describe("#ghPagesBadge", () => {
-    const repo = new Repo("MichaelCurrin", "badge-generator");
-
     it("returns a valid GitHub Pages badge", () => {
-      expect(repo.ghPagesBadge()).toBe(
+      expect(repoNoLicense.ghPagesBadge()).toBe(
         "[![View site - GH Pages](https://img.shields.io/badge/View_site-GH_Pages-2ea44f?style=for-the-badge)](https://michaelcurrin.github.io/badge-generator/)"
       );
     });
   });
 
   describe("#_templateURL", () => {
-    const repo = new Repo("MichaelCurrin", "badge-generator");
-
     it("returns a valid generate template URL", () => {
-      expect(repo._templateURL()).toBe(
+      expect(repoNoLicense._templateURL()).toBe(
         "https://github.com/MichaelCurrin/badge-generator/generate"
       );
     });
   });
 
   describe("#useThisTemplateBadge", () => {
-    const repo = new Repo("MichaelCurrin", "badge-generator");
-
     it("returns a valid generate template badge", () => {
-      expect(repo.useThisTemplateBadge()).toBe(
+      expect(repoNoLicense.useThisTemplateBadge()).toBe(
         "[![Use this template](https://img.shields.io/badge/Use_this_template-2ea44f?style=for-the-badge&logo=github)](https://github.com/MichaelCurrin/badge-generator/generate)"
       );
     });
   });
 
   describe("#licenseBadge", () => {
-    const repo = new Repo("MichaelCurrin", "badge-generator", "MIT");
-
     it("returns a badge for a local license", () => {
-      expect(repo.licenseBadge(true)).toBe(
+      expect(repoWithLicense.licenseBadge(true)).toBe(
         "[![License - MIT](https://img.shields.io/badge/License-MIT-blue)](#license)"
       );
     });
@@ -71,13 +69,11 @@ describe("#Repo", () => {
           "https://github.com/MichaelCurrin/badge-generator/blob/master/LICENSE",
         expectedBadge = `[![License - MIT](https://img.shields.io/badge/License-MIT-blue)](${target})`;
 
-      expect(repo.licenseBadge(false)).toBe(expectedBadge);
+      expect(repoWithLicense.licenseBadge(false)).toBe(expectedBadge);
     });
   });
 
   describe("#licenseMessage", () => {
-    const repo = new Repo("MichaelCurrin", "foo", "MIT");
-
     const message = `\
 ## License
 
@@ -85,7 +81,7 @@ Released under [MIT](/LICENSE) by [@MichaelCurrin](https://github.com/MichaelCur
 `;
 
     it("return a message for license", () => {
-      expect(repo.licenseMessage()).toBe(message);
+      expect(repoWithLicense.licenseMessage()).toBe(message);
     });
   });
 });
