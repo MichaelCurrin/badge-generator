1f5dafd1b6d1cf22b14ca34a146bb2a9e7e59c85
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-01-01 14:36:45 +0200

    style: Commas

--- src/core/Repo.ts
+++ src/core/Repo.ts
@@ -6,7 +6,7 @@ import {
   GH_BADGE,
   GH_PAGES_BADGE,
   LICENSE_BADGE,
-  TEMPLATE_BADGE
+  TEMPLATE_BADGE,
 } from "../constants/badgeValues";
 import {
   DEFAULT_BRANCH,
@@ -14,13 +14,13 @@ import {
   GITHUB_IO,
   LICENSE_PATH,
   SHIELDS_API,
-  VERSION_PARAMS
+  VERSION_PARAMS,
 } from "../constants/urls";
 import {
   buildUrl,
   genericBadge,
   markdownImageWithLink,
-  markdownLink
+  markdownLink,
 } from "./badges";
 import { TagTypes } from "./Repo.d";
 
