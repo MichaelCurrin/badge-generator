bdbbc730d93b93a0f38e2690d660ec4dda99a5e4
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-01-01 10:46:21 +0200

    test: Update Repo.spec.ts

--- tests/unit/core/Repo.spec.ts
+++ tests/unit/core/Repo.spec.ts
@@ -12,10 +12,25 @@ describe("#Repo", () => {
 
     it("return a badge for a remote license", () => {
       const target =
-          "https://github.com/MichaelCurrin/badge-generator/blob/master/LICENSE",
+        "https://github.com/MichaelCurrin/badge-generator/blob/master/LICENSE",
         expectedBadge = `[![License - MIT](https://img.shields.io/badge/License-MIT-blue)](${target})`;
 
       expect(repo.licenseBadge("MIT", false)).toBe(expectedBadge);
+    });
+  });
+
+  describe("#licenseMessage", () => {
+    const repo = new Repo("MichaelCurrin", "foo");
+
+    const message = `\
+## License
+
+Released under [MIT](/LICENSE) by [@MichaelCurrin](https://github.com/MichaelCurrin).
+`
+
+    it("return a message for license", () => {
+      expect(repo.licenseMessage("MIT")).toBe(message
+      );
     });
   });
 });
