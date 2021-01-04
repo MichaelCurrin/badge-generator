008909d5eb81ca20f1c3d2ba61ff1ace431a9cad
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-01-04 21:39:43 +0200

    fix: Update catalogue.ts

--- src/core/catalogue.ts
+++ src/core/catalogue.ts
@@ -2,15 +2,20 @@ import { BADGE_DEFAULTS, BADGE_DETAILS } from "../constants/catalogue";
 import { genericBadge } from "./genericBadge";
 
 export function renderBadges() {
-  return BADGE_DETAILS.map((badge) =>
-    genericBadge(
+  return BADGE_DETAILS.map((badge) => {
+    const isLarge =
+      typeof badge.isLarge === "undefined"
+        ? BADGE_DEFAULTS.IS_LARGE
+        : badge.isLarge;
+
+    return genericBadge(
       badge.label,
       badge.message,
       BADGE_DEFAULTS.COLOR,
-      badge.isLarge || BADGE_DEFAULTS.IS_LARGE,
+      isLarge,
       badge.target,
       badge.logo || "",
       badge.logo ? BADGE_DEFAULTS.LOGO_COLOR : ""
-    )
-  );
+    );
+  });
 }
