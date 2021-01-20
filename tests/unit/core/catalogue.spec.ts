11ed98e11733dbf7dba8cd001f8dba592cc31145
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-01-20 12:26:34 +0200

    test: Add tests to handle catalogue alt text

--- tests/unit/core/catalogue.spec.ts
+++ tests/unit/core/catalogue.spec.ts
@@ -1,6 +1,8 @@
 import { renderBadges } from "@/core/catalogue";
 
 describe("#renderBadges", () => {
+  // Just a sample of catalogue badges need to be tested for a variety of features. Not all badges.
+  // There are already tests for genericBadge function.
   it("converts configured catalogue badges to expected markdown badges", () => {
     const badges = renderBadges();
 
@@ -10,6 +12,10 @@ describe("#renderBadges", () => {
 
     expect(badges[1]).toBe(
       "[![OS - Linux](https://img.shields.io/badge/OS-Linux-blue?logo=linux&logoColor=white)](https://www.linux.org/)"
+    );
+
+    expect(badges[8]).toBe(
+      "[![Made with Python](https://img.shields.io/badge/Python->%3D3.6-blue?logo=python&logoColor=white)](https://python.org)"
     );
   });
 });
