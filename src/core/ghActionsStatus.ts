b5280fd82e17318a90aca079a6d3c748f0bae984
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-01-29 13:23:07 +0200

    style: Lint fix

--- src/core/ghActionsStatus.ts
+++ src/core/ghActionsStatus.ts
@@ -4,15 +4,18 @@
  * In GitHub on the Actions tab, go to a workflow run and under options on the right choose "Create status badge".
  * This is added to this generator project for convenience. Plus with a smart target URL added.
  */
+// TODO Move to related file or to a lib file that can be shared.
 type Repo = {
-  username: string
-  repoName: string
-}
+  username: string;
+  repoName: string;
+};
 
 export function statusData(repo: Repo, workflowName: string) {
   return {
-    altText: 'Deno CI',
-    imgUrl: 'https://github.com/MichaelCurrin/deno-project-template/workflows/Deno%20CI/badge.svg',
-    target: 'https://github.com/MichaelCurrin/deno-project-template/actions?query=workflow%3A%22Deno+CI%22'
-  }
+    altText: "Deno CI",
+    imgUrl:
+      "https://github.com/MichaelCurrin/deno-project-template/workflows/Deno%20CI/badge.svg",
+    target:
+      "https://github.com/MichaelCurrin/deno-project-template/actions?query=workflow%3A%22Deno+CI%22",
+  };
 }
--- tests/unit/core/ghActionsStatus.spec.ts
+++ tests/unit/core/ghActionsStatus.spec.ts
@@ -2,19 +2,20 @@ import { statusData } from "@/core/ghActionsStatus";
 
 describe("#ghActionsStatusBadge", () => {
   it("returns a valid CI badge with a link", () => {
-
     const repo = {
       username: "MichaelCurrin",
-      repoName: 'deno-project-template'
-    }
+      repoName: "deno-project-template",
+    };
     // This be the `name` field at the top of your YAML file. The file name is irrelevant.
-    const workflowName = 'Deno CI'
+    const workflowName = "Deno CI";
 
     const expected = {
-      altText: 'Deno CI',
-      imgUrl: 'https://github.com/MichaelCurrin/deno-project-template/workflows/Deno%20CI/badge.svg',
-      target: 'https://github.com/MichaelCurrin/deno-project-template/actions?query=workflow%3A%22Deno+CI%22'
-    }
+      altText: "Deno CI",
+      imgUrl:
+        "https://github.com/MichaelCurrin/deno-project-template/workflows/Deno%20CI/badge.svg",
+      target:
+        "https://github.com/MichaelCurrin/deno-project-template/actions?query=workflow%3A%22Deno+CI%22",
+    };
 
     expect(statusData(repo, workflowName)).toStrictEqual(expected);
   });
