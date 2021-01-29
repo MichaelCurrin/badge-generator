dbb6534759257e637cda99ac3cf61e95dc1b76a5
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-01-29 13:31:44 +0200

    feat: Make repo dynamic

--- src/core/ghActions.ts
+++ src/core/ghActions.ts
@@ -4,21 +4,25 @@
  * In GitHub on the Actions tab, go to a workflow run and under options on the right choose "Create status badge".
  * This is added to this generator project for convenience. Plus with a smart target URL added.
  */
+
+import { Repo } from "./Repo";
+
 // TODO Move to related file or to a lib file that can be shared.
-type Repo = {
+type TRepo = {
   username: string;
   repoName: string;
 };
 
-export function statusData(repo: Repo, workflowName: string) {
+export function statusData(repoFields: TRepo, workflowName: string) {
   // The URL encoded target comes from search in the UI like 'workflow:"Deno CI"'.
+
+  const repo = new Repo(repoFields.username, repoFields.repoName);
+  const ghURL = repo.ghURL();
+
   return {
     altText: workflowName,
-    imgUrl: `https://github.com/MichaelCurrin/deno-project-template/workflows/${workflowName.replace(
-      " ",
-      "%20"
-    )}/badge.svg`,
-    target: `https://github.com/MichaelCurrin/deno-project-template/actions?query=workflow%3A%22${workflowName.replace(
+    imgUrl: `${ghURL}/workflows/${workflowName.replace(" ", "%20")}/badge.svg`,
+    target: `${ghURL}/actions?query=workflow%3A%22${workflowName.replace(
       " ",
       "+"
     )}%22`,
