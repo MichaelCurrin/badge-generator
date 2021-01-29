556129b5a33775b29dba3a957a51b9a13865066f
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-01-29 13:38:10 +0200

    refactor: Create _statusTargetUrl function

--- src/core/ghActions.ts
+++ src/core/ghActions.ts
@@ -13,22 +13,38 @@ type TRepo = {
   repoName: string;
 };
 
+/**
+ * Create URL for a GH Actions CI status badge.
+ */
 export function _statusBadgeUrl(ghURL: string, workflowName: string) {
-  return `${ghURL}/workflows/${workflowName.replace(" ", "%20")}/badge.svg`;
+  const encodedName = workflowName.replace(" ", "%20");
+
+  return `${ghURL}/workflows/${encodedName}/badge.svg`;
 }
 
-export function statusData(repoFields: TRepo, workflowName: string) {
-  // The URL encoded target comes from search in the UI like 'workflow:"Deno CI"'.
+/**
+ * Create URL to view runs for a workflow name.
+ *
+ * The URL encoded target comes from search in the UI like 'workflow:"Deno CI"'.
+ *
+ * TODO refactor use to clean URL and then apply encoding to it.
+ */
+export function _statusTargetUrl(ghURL: string, workflowName: string) {
+  const encodedName = workflowName.replace(" ", "+");
 
+  return `${ghURL}/actions?query=workflow%3A%22${encodedName}%22`;
+}
+
+/**
+ * Return data needed to make a GH Actions CI badge.
+ */
+export function statusData(repoFields: TRepo, workflowName: string) {
   const repo = new Repo(repoFields.username, repoFields.repoName);
   const ghURL = repo.ghURL();
 
   return {
     altText: workflowName,
     imgUrl: _statusBadgeUrl(ghURL, workflowName),
-    target: `${ghURL}/actions?query=workflow%3A%22${workflowName.replace(
-      " ",
-      "+"
-    )}%22`,
+    target: _statusTargetUrl(ghURL, workflowName),
   };
 }
