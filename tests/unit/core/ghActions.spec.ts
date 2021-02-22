d48faac3c9efed9ebcfe1e160afaa4b91cb7ca24
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-02-22 22:23:43 +0200

    test: Update ghActions.spec.ts

--- tests/unit/core/ghActions.spec.ts
+++ tests/unit/core/ghActions.spec.ts
@@ -2,7 +2,20 @@ import { _statusData } from "@/core/ghActions";
 
 describe("#_statusData", () => {
   describe("returns a valid CI badge with a link", () => {
-    it("handles a Deno CI for deno-project-template", () => {
+    it("handles a foo bar CI project", () => {
+      const ghUrl = "https://github.com/foo/bar"
+      const workflowName = "Bazz CI";
+
+      const expected = {
+        altText: "Bazz CI",
+        imgUrl: "https://github.com/foo/bar/workflows/Bazz%20CI/badge.svg",
+        target: 'https://github.com/foo/bar/actions?query=workflow:"Bazz+CI"',
+      };
+
+      expect(_statusData(ghUrl, workflowName)).toStrictEqual(expected);
+    });
+
+    it("handles Deno CI for deno-project-template", () => {
       const ghUrl = "https://github.com/MichaelCurrin/deno-project-template"
       const workflowName = "Deno CI";
 
@@ -12,19 +25,6 @@ describe("#_statusData", () => {
           "https://github.com/MichaelCurrin/deno-project-template/workflows/Deno%20CI/badge.svg",
         target:
           'https://github.com/MichaelCurrin/deno-project-template/actions?query=workflow:"Deno+CI"',
-      };
-
-      expect(_statusData(ghUrl, workflowName)).toStrictEqual(expected);
-    });
-
-    it("handles a foo bar project", () => {
-      const ghUrl = "https://github.com/foo/bar"
-      const workflowName = "Bazz CI";
-
-      const expected = {
-        altText: "Bazz CI",
-        imgUrl: "https://github.com/foo/bar/workflows/Bazz%20CI/badge.svg",
-        target: 'https://github.com/foo/bar/actions?query=workflow:"Bazz+CI"',
       };
 
       expect(_statusData(ghUrl, workflowName)).toStrictEqual(expected);
