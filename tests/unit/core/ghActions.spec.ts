4f7c92e8ec1683631ae5c8cbf4e040564384335f
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-02-22 22:22:25 +0200

    test: Update to match new _statusData flow and refactor to use extra describe

--- tests/unit/core/ghActions.spec.ts
+++ tests/unit/core/ghActions.spec.ts
@@ -1,40 +1,33 @@
-import { statusData } from "@/core/ghActions";
+import { _statusData } from "@/core/ghActions";
 
-describe("#statusData", () => {
-  it("returns a valid CI badge with a link", () => {
-    const repo = {
-      username: "MichaelCurrin",
-      repoName: "deno-project-template",
-    };
-    // This field comes from the `name` value at the top of your YAML file. The actual filename is
-    // irrelevant.
-    const workflowName = "Deno CI";
+describe("#_statusData", () => {
+  describe("returns a valid CI badge with a link", () => {
+    it("handles a Deno CI for deno-project-template", () => {
+      const ghUrl = "https://github.com/MichaelCurrin/deno-project-template"
+      const workflowName = "Deno CI";
 
-    const expected = {
-      altText: "Deno CI",
-      imgUrl:
-        "https://github.com/MichaelCurrin/deno-project-template/workflows/Deno%20CI/badge.svg",
-      target:
-        'https://github.com/MichaelCurrin/deno-project-template/actions?query=workflow:"Deno+CI"',
-    };
+      const expected = {
+        altText: "Deno CI",
+        imgUrl:
+          "https://github.com/MichaelCurrin/deno-project-template/workflows/Deno%20CI/badge.svg",
+        target:
+          'https://github.com/MichaelCurrin/deno-project-template/actions?query=workflow:"Deno+CI"',
+      };
 
-    expect(statusData(repo, workflowName)).toStrictEqual(expected);
-  });
+      expect(_statusData(ghUrl, workflowName)).toStrictEqual(expected);
+    });
 
-  it("returns a valid CI badge with a link", () => {
-    const repo = {
-      username: "foo",
-      repoName: "bar",
-    };
-    // This comes from the `name` field at the top of your YAML file. The file name is irrelevant.
-    const workflowName = "Bazz CI";
+    it("returns a valid CI badge with a link", () => {
+      const ghUrl = "https://github.com/foo/bar"
+      const workflowName = "Bazz CI";
 
-    const expected = {
-      altText: "Bazz CI",
-      imgUrl: "https://github.com/foo/bar/workflows/Bazz%20CI/badge.svg",
-      target: 'https://github.com/foo/bar/actions?query=workflow:"Bazz+CI"',
-    };
+      const expected = {
+        altText: "Bazz CI",
+        imgUrl: "https://github.com/foo/bar/workflows/Bazz%20CI/badge.svg",
+        target: 'https://github.com/foo/bar/actions?query=workflow:"Bazz+CI"',
+      };
 
-    expect(statusData(repo, workflowName)).toStrictEqual(expected);
+      expect(_statusData(ghUrl, workflowName)).toStrictEqual(expected);
+    });
   });
 });
