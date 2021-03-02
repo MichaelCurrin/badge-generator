b091ff7e1bc3a0039d0f22e5611cfffff403bfa6
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-03-02 11:24:23 +0200

    feat: Add disabled flag to logo color

--- src/views/GenericBadges.vue
+++ src/views/GenericBadges.vue
@@ -45,6 +45,7 @@
               <TextInput
                 label="Logo color"
                 v-model="logoColor"
+                :disabled="logo === ''"
                 note="The badge's own color will be used unless you specify an override. Using `white` is great for readability against the dark label background."
               />
             </fieldset>
