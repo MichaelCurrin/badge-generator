92f62c5b87a7be8ce653ed8c179b6f0d4c6c2f66
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-03-04 20:44:45 +0200

    docs

--- src/components/Markdown.vue
+++ src/components/Markdown.vue
@@ -6,8 +6,6 @@
 import Vue from "vue";
 import markdownIt from "markdown-it";
 
-// TODO: The docstring says rendering from HTML is unsafe and recommends sanitizing, or using a
-// plugin. No so important for this app as it has no persistence layer, but maybe research one day.
 const md = new markdownIt({ html: true });
 
 export default Vue.extend({
