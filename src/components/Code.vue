d08c5c8652ab734d01e994ab3d4414f8a884921b
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-03-05 10:21:20 +0200

    feat: Create Code.vue

---
+++ src/components/Code.vue
@@ -0,0 +1,20 @@
+<template>
+  <highlightjs :language="language" :code="code" />
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import hljs from "highlight.js";
+
+Vue.use(hljs.vuePlugin);
+
+const defaultLanguage = "markdown";
+
+export default Vue.extend({
+  name: "Code",
+  props: {
+    code: { type: String, required: true },
+    language: { type: String, required: false, default: defaultLanguage },
+  },
+});
+</script>
