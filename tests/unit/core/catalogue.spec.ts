79c11f2166ebf80ec75722a88ff690273090e02d
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-03-28 22:02:49 +0200

    test: Update and add cases

--- tests/unit/core/catalogue.spec.ts
+++ tests/unit/core/catalogue.spec.ts
@@ -15,7 +15,11 @@ describe("#renderBadges", () => {
     );
 
     expect(badges[8]).toBe(
-      "[![Made with Python](https://img.shields.io/badge/Python->%3D3.6-blue?logo=python&logoColor=white)](https://python.org)"
+      "[![Made with Python](https://img.shields.io/badge/Python->=3.6-blue?logo=python&logoColor=white)](https://python.org)"
+    );
+
+    expect(badges[7]).toBe(
+      "[![Made with Bash](https://img.shields.io/badge/Bash->=3-blue?logo=gnu-bash&logoColor=white)](https://www.gnu.org/software/bash/)"
     );
   });
 });
