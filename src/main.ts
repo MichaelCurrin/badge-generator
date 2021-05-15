960887d522f92bc8976f06e45bcdf580ee228600
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-05-15 11:10:48 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
-import Vue from "vue";
+// import hljs from "highlight.js";
+// import { createApp } from "vue";
+// import App from "./App.vue";
+// import router from "./router";
+
+// const app = createApp(App);
+// app.use(router);
+// app.use(hljs.vuePlugin);
+// app.mount("#app");
+
+import hljs from "highlight.js/lib/common";
+import vuePlugin from "@highlightjs/vue-plugin";
+
+import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-Vue.config.productionTip = false;
-
-new Vue({
-  router,
-  render: (h) => h(App),
-}).$mount("#app");
+const app = createApp(App);
+app.use(router);
+app.use(vuePlugin);
+app.mount("#app");
