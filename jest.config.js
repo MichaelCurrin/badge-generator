07b51dc0df9b8c9c10bd35fea81c4bcbde4c6051
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-05-15 18:41:37 +0200

    chore: Fix tests

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest/presets/typescript",
   testPathIgnorePatterns: ["<rootDir>/dist/"],
+  transform: {
+    "^.+\\.vue$": "vue-jest",
+  },
 };
