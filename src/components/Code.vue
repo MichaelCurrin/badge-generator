958d1727a1f886b0fae1b13fdfc896e0a4a31aa1
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-05-15 21:57:31 +0200

    feat: Add syntax highlighting to Code.vue

--- src/components/Code.vue
+++ src/components/Code.vue
@@ -1,14 +1,27 @@
 <template>
-  <pre><code>{{ code }}</code></pre>
+  <pre><code ref="codeBlock" class="markdown">{{ code }}</code></pre>
 </template>
 
 <script lang="ts">
+import hljs from "highlight.js";
 import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "Code",
   props: {
     code: { type: String, required: true },
+  },
+  methods: {
+    highlight() {
+      const block = this.$refs.codeBlock as HTMLElement;
+      hljs.highlightBlock(block);
+    },
+  },
+  mounted() {
+    this.highlight();
+  },
+  updated() {
+    this.highlight();
   },
 });
 </script>
