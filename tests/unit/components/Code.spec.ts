3031251f935a8ba5319cd9aa46a992c7aae75fbc
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-05-15 22:07:01 +0200

    test: Update Code.spec.ts

--- tests/unit/components/Code.spec.ts
+++ tests/unit/components/Code.spec.ts
@@ -2,8 +2,8 @@ import Code from "@/components/Code.vue";
 import { shallowMount } from "@vue/test-utils";
 
 describe("Code.vue", () => {
-  // Note that we do not care about convering Markdown to HTML here. That is supposed to be
-  // triggered but doesn't seem to happen in tests.
+  // Note that we do not care about convering Markdown to HTML here. It doesn't get rendered when
+  // doing the highlightjs component or using the plain style.
   it("renders a codeblock with highlighting", () => {
     const wrapper = shallowMount(Code, {
       props: {
