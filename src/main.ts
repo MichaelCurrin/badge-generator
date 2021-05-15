d57e82ecb3c992299b2e524f8b12c8821beb1f0a
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-05-15 21:57:42 +0200

    feat: Add syntax highlighting to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
+import hljs from "highlight.js";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
 const app = createApp(App);
 app.use(router);
+app.use(hljs.vuePlugin);
 app.mount("#app");
