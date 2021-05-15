5036f3ba18977504985acee2f8bda58658089a29
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-05-15 12:17:17 +0200

    refactor: Remove unused value

--- src/components/Code.vue
+++ src/components/Code.vue
@@ -6,13 +6,10 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 
-const defaultLanguage = "markdown";
-
 export default defineComponent({
   name: "Code",
   props: {
     code: { type: String, required: true },
-    language: { type: String, required: false, default: defaultLanguage },
   },
 });
 </script>
