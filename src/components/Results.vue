ce171a9905fa466d25333680e8697503f39be0b9
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-05-15 12:19:09 +0200

    style: Update Results.vue

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,17 +1,19 @@
 <template>
-  <!-- Badge generator ouput pane -->
+  <!-- Badge generator output pane -->
   <div class="app-results">
     <h2>Output values</h2>
 
     <div>
       <h3>Preview</h3>
       <!-- Make sure to not add result on a new line otherwise it will indent and format. -->
-      <!-- Make sure to use :source since {{ result }} as inner text will fail to update on UI changes. -->
+      <!-- Make sure to use :source since {{ result }} as inner text will fail to update on UI
+        changes. -->
       <Markdown :content="result"></Markdown>
     </div>
 
     <div>
       <h3>Code</h3>
+
       <Code :code="result" />
     </div>
   </div>
