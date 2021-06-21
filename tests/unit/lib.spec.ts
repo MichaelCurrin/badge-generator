a6b983a4d5f19184cab5fbb813a01cfb4da26bc6
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-06-21 13:54:55 +0200

    test: update lib.spec.ts with cases

--- tests/unit/lib.spec.ts
+++ tests/unit/lib.spec.ts
@@ -1,4 +1,4 @@
-import { stripLeadingSlash } from "@/lib";
+import { slugify, stripLeadingSlash } from "@/lib";
 
 describe("#stripLeadingSlash", () => {
   it("removes a leading slash", () => {
@@ -11,3 +11,33 @@ describe("#stripLeadingSlash", () => {
     expect(stripLeadingSlash("foo/bar.png")).toBe("foo/bar.png");
   });
 });
+
+
+describe("#slugify", () => {
+  describe("creates a slug correctly", () => {
+    it("trims", () => {
+      expect(slugify(' abcdef ')).toBe("abcdef")
+    })
+
+    it("downcases", () => {
+      expect(slugify('ABCDEF')).toBe("abcdef")
+    })
+
+    it("removes non-alphanumeric characters", () => {
+      expect(slugify('!@#$%123')).toBe("123")
+    })
+
+    it("collapses whitespaces into a single dash", () => {
+      expect(slugify('a b')).toBe("a-b")
+      expect(slugify('a   b')).toBe("a-b")
+    })
+
+    it("collapses dashes into a single dash", () => {
+      expect(slugify('a--b')).toBe("a-b")
+    })
+
+    it("handles a mix", () => {
+      expect(slugify('ABC DEF ? 123 xyz')).toBe("abc-def-123-xyz")
+    })
+  })
+})
