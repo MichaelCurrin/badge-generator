ba3c6414529f908684250c508ea93c71698b1c7a
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-06-21 14:16:26 +0200

    refactor: update Code.spec.ts

--- tests/unit/components/Code.spec.ts
+++ tests/unit/components/Code.spec.ts
@@ -9,7 +9,8 @@ describe("Code.vue", () => {
       },
     });
     const expected =
-      '<pre><code class="markdown hljs"><span class="hljs-section">## Foo bar</span></code></pre>';
+      '<pre><code class="markdown hljs"><span class="hljs-section">## Foo bar</span></code></pre>'
+
     expect(wrapper.html()).toContain(expected);
   });
 
@@ -25,10 +26,23 @@ describe("Code.vue", () => {
       }
     });
     const expected = `\
-<div>
-  <checkbox-stub label="Show HTML" modelvalue="true"></checkbox-stub><br><pre><code class="markdown hljs"><span class="xml"><span class="hljs-tag">&lt;<span class="hljs-name">h2</span>&gt;</span></span>Foo bar<span class="xml"><span class="hljs-tag">&lt;/<span class="hljs-name">h2</span>&gt;</span></span>
-</code></pre>
-</div>`;
+<pre><code class="markdown hljs"><span class="xml"><span class="hljs-tag">&lt;<span class="hljs-name">h2</span>&gt;</span></span>Foo bar<span class="xml"><span class="hljs-tag">&lt;/<span class="hljs-name">h2</span>&gt;</span></span>
+</code></pre>`
+
     expect(wrapper.html()).toContain(expected);
+  });
+
+  it("renders a show HTML checkbox", () => {
+    const wrapper = shallowMount(Code, {
+      props: {
+        code: "## Foo bar",
+      },
+    });
+    const expected = `\
+<div>
+  <checkbox-stub label="Show HTML" modelvalue="false"></checkbox-stub><br><pre><code class="markdown hljs"><span class="hljs-section">## Foo bar</span></code></pre>
+</div>`
+
+    expect(wrapper.html()).toBe(expected);
   });
 });
