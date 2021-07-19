7bda587dc56b163449781e599d48b88f64d5770d
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-07-19 10:58:31 +0200

    style: update Repo.ts

--- src/core/Repo.ts
+++ src/core/Repo.ts
@@ -9,7 +9,7 @@ import {
   GH_BADGE,
   GH_PAGES_BADGE,
   LICENSE_BADGE,
-  TEMPLATE_BADGE
+  TEMPLATE_BADGE,
 } from "@/constants/badgeValues";
 import {
   DEFAULT_BRANCH,
@@ -17,7 +17,7 @@ import {
   GITHUB_IO,
   LICENSE_PATH,
   SHIELDS_API,
-  VERSION_PARAMS
+  VERSION_PARAMS,
 } from "@/constants/urls";
 import { buildUrl } from "./badges";
 import { genericBadge } from "./genericBadge";
