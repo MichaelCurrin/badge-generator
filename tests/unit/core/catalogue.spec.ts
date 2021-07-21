7fde1c070d600e1f06d5ed4a97050a9d390564f4
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-07-21 12:31:35 +0200

    test: update catalogue.spec.ts

--- tests/unit/core/catalogue.spec.ts
+++ tests/unit/core/catalogue.spec.ts
@@ -3,23 +3,31 @@ import { renderBadges } from "@/core/catalogue";
 describe("#renderBadges", () => {
   // Just a sample of catalogue badges need to be tested for a variety of features. Not all badges.
   // There are already tests for genericBadge function.
-  it("converts configured catalogue badges to expected markdown badges", () => {
+  describe("convert configured catalogue badges to expected markdown badges", () => {
     const badges = renderBadges();
 
-    expect(badges[0]).toBe(
-      "[![View - Documentation](https://img.shields.io/badge/View-Documentation-blue?style=for-the-badge)](/docs/)"
-    );
+    it("renders a docs badge", () => {
+      expect(badges[0]).toBe(
+        "[![view - Documentation](https://img.shields.io/badge/view-Documentation-blue?style=for-the-badge)](/docs/)"
+      );
+    })
 
-    expect(badges[1]).toBe(
-      "[![OS - Linux](https://img.shields.io/badge/OS-Linux-blue?logo=linux&logoColor=white)](https://www.linux.org/)"
-    );
+    it("renders a Linux badge", () => {
+      expect(badges[3]).toBe(
+        "[![OS - Linux](https://img.shields.io/badge/OS-Linux-blue?logo=linux&logoColor=white)](https://www.linux.org/)"
+      );
+    })
 
-    expect(badges[8]).toBe(
-      "[![Made with Python](https://img.shields.io/badge/Python->=3.6-blue?logo=python&logoColor=white)](https://python.org)"
-    );
+    it("renders a Bash badge", () => {
+      expect(badges[9]).toBe(
+        "[![Made with Bash](https://img.shields.io/badge/Bash->=3-blue?logo=gnu-bash&logoColor=white)](https://www.gnu.org/software/bash/)"
+      );
+    });
 
-    expect(badges[7]).toBe(
-      "[![Made with Bash](https://img.shields.io/badge/Bash->=3-blue?logo=gnu-bash&logoColor=white)](https://www.gnu.org/software/bash/)"
-    );
+    it("renders a Python badge", () => {
+      expect(badges[10]).toBe(
+        "[![Made with Python](https://img.shields.io/badge/Python->=3.6-blue?logo=python&logoColor=white)](https://python.org)"
+      );
+    })
   });
-});
+})
