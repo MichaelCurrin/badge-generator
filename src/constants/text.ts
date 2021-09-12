13c1cec95f8706a750ae67716e75e22d6e8fcb11
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-09-12 18:28:15 +0200

    style: update text.ts

--- src/constants/text.ts
+++ src/constants/text.ts
@@ -1,3 +1,4 @@
 export const INITIAL_RESULT = "_Your output will appear here_";
 
-export const COLOR_HELP = "Hints: `brightgreen`, `green`, `yellowgreen`, `yellow`, `orange`, `red`, `blue`, `lightgrey`, `success`, `important`, `critical`, `informational`, `inactive`, `blueviolet`, `ff69b4`, `9cf`"
+export const COLOR_HELP =
+  "Hints: `brightgreen`, `green`, `yellowgreen`, `yellow`, `orange`, `red`, `blue`, `lightgrey`, `success`, `important`, `critical`, `informational`, `inactive`, `blueviolet`, `ff69b4`, `9cf`";
