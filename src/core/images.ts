88d3ebd986c87654c299637857e6b53c6b561cf1
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-09-12 17:45:43 +0200

    docs: update images.ts

--- src/core/images.ts
+++ src/core/images.ts
@@ -1,11 +1,13 @@
 /**
  * Images module.
  *
- * This was attempted using a Vue component instead of with templating. But rendering proved an
- * issue as the finished element was shown even inside of `pre` and `code` tags.
+ * This was attempted using a Vue component instead of with templating. But
+ * rendering proved an issue as the finished element was shown even inside of
+ * `pre` and `code` tags.
  *
- * The indentation here is not smart because of the nested rendering, but, it works well enough to
- * use it.
+ * The indentation here is not smart because of the nested rendering, but, it
+ * works well enough to use it. Maybe there is a function in the Markdown
+ * library or similar to handle indenting.
  */
 export function mkHtmlImg(
   src: string,
