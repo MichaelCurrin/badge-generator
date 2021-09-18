b380de8b80bf6046246d3915f08ed0a3dee3490f
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-09-18 13:09:24 +0200

    feat: update help

--- src/constants/text.ts
+++ src/constants/text.ts
@@ -1,10 +1,10 @@
 export const INITIAL_RESULT = "_Your output will appear here_";
 
 export const COLOR_HELP =
-  "Hints: `brightgreen`, `green`, `yellowgreen`, `yellow`, `orange`, `red`, `blue`, `lightgrey`, `success`, `important`, `critical`, `informational`, `inactive`, `blueviolet`, `ff69b4`, `9cf`, `2ea44f` (GitHub's green)";
+  "Color keyword or hex code. Hints: `brightgreen`, `green`, `yellowgreen`, `yellow`, `orange`, `red`, `blue`, `lightgrey`, `success`, `important`, `critical`, `informational`, `inactive`, `blueviolet`, `ff69b4`, `9cf`, `2ea44f` (GitHub's green). The Shields.io API will use `blue` as a default if no color is provided.";
 
 export const LOGO_HELP =
   "Supports values like `dependabot`, `discord` and `npm`. And hundreds of logos from [SimpleIcons](https://simpleicons.org/) such as `python`, `node.js` and `visual-studio-code`";
 
 export const LOGO_COLOR_HELP =
-  "The badge's own color will be used unless you specify an override. Using `white` is great for readability against the dark label background.";
+  "Color keyword or hex code. The badge's own color will be used unless you specify an override. Using `white` is great for readability against the dark label background.";
