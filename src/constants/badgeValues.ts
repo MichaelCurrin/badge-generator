f91ee4ece4d854167505fec3765b3761abc6d795
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-10-31 16:18:12 +0200

    docs: update badgeValues.ts

--- src/constants/badgeValues.ts
+++ src/constants/badgeValues.ts
@@ -1,11 +1,11 @@
 /**
  * Badge values constants module.
  *
- * The aim is to move fixed values out of scripts where they are used and rather move them to a
- * central script here which is like a config.
+ * The aim is to move fixed values out of scripts where they are used and rather
+ * move them to a central script here which is like a config.
  *
- * All the values set here are fixed or are defaults. Any values not fixed here will be set by the
- * user.
+ * All the values set here are fixed or are defaults. Any values not fixed here
+ * will be set by the user.
  */
 import { COLOR_PRESETS } from "./appearance";
 import { IGenericBadge } from "./badgeValues.d";
@@ -49,9 +49,9 @@ export const GH_PAGES_BADGE: IGenericBadge = {
   isLarge: true,
 };
 
-// Keep the message to match what GitHub's own template badge says, so it makes it easier for
-// someone to predict what the button will do. Logo was GitHub before, but it took up too much space
-// and was distracting.
+// Keep the message to match what GitHub's own template badge says, so it makes
+// it easier for someone to predict what the button will do. Logo was GitHub
+// before, but it took up too much space and was distracting.
 export const TEMPLATE_BADGE: IGenericBadge = {
   label: "Generate",
   message: "Use this template",
