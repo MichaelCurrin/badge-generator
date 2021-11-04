f4b1f5d3081a3ee96cdc1775c0ac49c51396ffc0
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-11-04 12:04:51 +0200

    test: update markdown.spec.ts

--- tests/unit/core/markdown.spec.ts
+++ tests/unit/core/markdown.spec.ts
@@ -11,20 +11,21 @@ describe("#mdLink", () => {
 });
 
 describe("#mdImage", () => {
-  it("returns a valid markdown image", () => {
-    expect(mdImage({ altText: "Alt text", imageTarget: "foo.png" })).toBe(
-      "![Alt text](foo.png)"
-    );
-
-    expect(
-      mdImage({
+  it("returns a valid Markdown image element", () => {
+    {
+      const result = mdImage({ altText: "Alt text", imageTarget: "foo.png" });
+      expect(result).toBe("![Alt text](foo.png)");
+    }
+    {
+      const result = mdImage({
         altText: "Example",
         imageTarget: "https://example.com/foo.png",
-      })
-    ).toBe("![Example](https://example.com/foo.png)");
+      });
+      expect(result).toBe("![Example](https://example.com/foo.png)");
+    }
   });
 
-  it("returns a valid markdown image with hover text", () => {
+  it("returns a valid Markdown image with hover title set", () => {
     expect(
       mdImage({
         altText: "Alt text",
@@ -44,7 +45,25 @@ describe("#mdImage", () => {
 });
 
 describe("#mdImageWithLink", () => {
-  it("returns a valid tag using all parameters set", () => {
+  it("returns a valid element using required parameters set", () => {
+    expect(
+      mdImageWithLink({
+        altText: "My alt text",
+        imageTarget: "foo.png",
+        linkTarget: "https://example.com",
+      })
+    ).toBe("[![My alt text](foo.png)](https://example.com)");
+
+    expect(
+      mdImageWithLink({
+        altText: "My title",
+        imageTarget: "/example.png",
+        linkTarget: "https://example.com",
+      })
+    ).toBe("[![My title](/example.png)](https://example.com)");
+  });
+
+  it("returns a valid element using hover title set", () => {
     expect(
       mdImageWithLink({
         altText: "My alt text",
@@ -59,11 +78,12 @@ describe("#mdImageWithLink", () => {
         altText: "My title",
         imageTarget: "/example.png",
         linkTarget: "https://example.com",
+        hoverTitle: "My hover title",
       })
-    ).toBe("[![My title](/example.png)](https://example.com)");
+    ).toBe('[![My title](/example.png)](https://example.com "My hover title")');
   });
 
-  it("Does not encode special characters", () => {
+  it("Returns a valid element with special characters preserved", () => {
     expect(
       mdImageWithLink({
         altText: "My title",
