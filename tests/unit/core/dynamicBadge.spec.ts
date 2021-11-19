8cb031dad847a9b1108e99d3698000a816b2e5a1
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-11-19 12:03:05 +0200

    test: update dynamicBadge.spec.ts

--- tests/unit/core/dynamicBadge.spec.ts
+++ tests/unit/core/dynamicBadge.spec.ts
@@ -36,12 +36,30 @@ describe("Dynamic data badges", () => {
         "https://raw.githubusercontent.com/MichaelCurrin/auto-commit-msg/master/package.json";
       const query = "$.version";
 
-      it("displays a badge pointing to an external link when a link is given", () => {
-        const linkTarget = "https://example.com";
+      const linkTarget = "https://example.com";
+      const altText = "My alt text";
+
+      it("returns a that uses an external link when one is given", () => {
         const expected =
           "[![version](https://img.shields.io/badge/dynamic/json?label=version&query=%24.version&url=https%3A%2F%2Fraw.githubusercontent.com%2FMichaelCurrin%2Fauto-commit-msg%2Fmaster%2Fpackage.json)](https://example.com)";
 
         expect(dynamicBadge(label, url, query, linkTarget)).toBe(expected);
+      });
+
+      it("returns a badge with image alt text when alt text is given", () => {
+        const expected =
+          "![My alt text](https://img.shields.io/badge/dynamic/json?label=version&query=%24.version&url=https%3A%2F%2Fraw.githubusercontent.com%2FMichaelCurrin%2Fauto-commit-msg%2Fmaster%2Fpackage.json)";
+
+        expect(dynamicBadge(label, url, query, "", altText)).toBe(expected);
+      });
+
+      it("returns a badge with alt text and external link when both are given", () => {
+        const expected =
+          "[![My alt text](https://img.shields.io/badge/dynamic/json?label=version&query=%24.version&url=https%3A%2F%2Fraw.githubusercontent.com%2FMichaelCurrin%2Fauto-commit-msg%2Fmaster%2Fpackage.json)](https://example.com)";
+
+        expect(dynamicBadge(label, url, query, linkTarget, altText)).toBe(
+          expected
+        );
       });
     });
   });
