fcd7743b259cba80bff2e84ac730d29f2dcfe60c
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-11-20 16:32:45 +0200

    test: update store.spec.ts

--- tests/unit/store.spec.ts
+++ tests/unit/store.spec.ts
@@ -3,26 +3,26 @@ import store from "@/store";
 describe("store", () => {
   describe("defaults", () => {
     it("has default values on initialization", () => {
-      expect(store.state.repoUsername).toBe("MichaelCurrin");
-      expect(store.state.repoName).toBe("badge-generator");
+      expect(store.getRepoUsername()).toBe("MichaelCurrin");
+      expect(store.getRepoName()).toBe("badge-generator");
     });
   });
 
   describe("#setRepoUsername", () => {
     it("can set and persist username", () => {
-      expect(store.state.repoUsername).toBe("MichaelCurrin");
+      expect(store.getRepoUsername()).toBe("MichaelCurrin");
 
       store.setRepoUsername("FooBar");
-      expect(store.state.repoUsername).toBe("FooBar");
+      expect(store.getRepoUsername()).toBe("FooBar");
     });
   });
 
   describe("#setRepoName", () => {
     it("can set and perist repo name", () => {
-      expect(store.state.repoName).toBe("badge-generator");
+      expect(store.getRepoName()).toBe("badge-generator");
 
       store.setRepoName("fizz-buzz");
-      expect(store.state.repoName).toBe("fizz-buzz");
+      expect(store.getRepoName()).toBe("fizz-buzz");
     });
   });
 });
