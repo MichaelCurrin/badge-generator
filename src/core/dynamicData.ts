6d78613a0e5c6d8b10b4ad917271178e18292ddc
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2022-01-05 20:37:47 +0200

    docs: update dynamicData.ts

--- src/core/dynamicData.ts
+++ src/core/dynamicData.ts
@@ -5,7 +5,8 @@ import { dynamicParamsUrl } from "./shieldsApi";
  * Dynamic data badge.
  *
  * Generate a badge that references a value in a data file - useful for both
- * static values and for values that change.
+ * static values and for values that change where a live-updating badge is
+ * useful.
  *
  * Only JSON is supported here or now but Shields.io also supports YAML and XML.
  */
