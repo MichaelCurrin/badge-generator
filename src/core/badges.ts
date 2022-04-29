15ea1fb9db66d20aadf3ca9b096efbf019e87b2b
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2022-04-29 14:27:59 +0200

    docs: update badges.ts

--- src/core/badges.ts
+++ src/core/badges.ts
@@ -3,8 +3,10 @@ import { StrMap } from "./types.d";
 /**
  * Create a serialized string from a base URL and query params.
  *
- * @param url Protocol, domain and path. This must have a protocol or it will be considered invalid.
- * @param queryParams Object of key-value pairs. This will be outputted in the form: '?foo=bar'.
+ * @param url Protocol, domain and path. This must have a protocol or it will be
+ *   considered invalid.
+ * @param queryParams Object of key-value pairs. This will be outputted in the
+ *   form: '?foo=bar'.
  */
 export function buildUrl(url: string, queryParams: StrMap): string {
   const urlObj = new URL(url);
