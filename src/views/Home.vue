ee056ce007fc3c79e6712748e470f1893e75c6bb
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2025-02-26 11:30:38 +0100

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,6 @@
     <div class="row">
       <div class="col-12">
         <h1>Home</h1>
-
         <!-- It would look better on page load to use height=250, except that on mobile the image
         gets squashed. The image needs to actually get a smaller height when the width is made to be
         smaller and CSS can't help here it seems. Width was set before in HTML but now it converts
@@ -14,11 +13,8 @@
             :src="`${baseUrl}hero.jpeg`"
             alt="Website banner logo"
           />
-
           <br />
-
           <Markdown :content="repoBadge"></Markdown>
-
           <p>
             <i>{{ description }}</i>
           </p>
@@ -30,17 +26,16 @@
       <div class="col-12">
         <p>
           This is an online tool which generates badges (or "shields") based on
-          your inputs. You can preview the badges on the tool and then copy the
-          Markdown/HTML to your GitHub repo's
-          <b>README.md</b> file. Those metadata badges can help people learn
+          your inputs, using the <a href="https://shields.io"/>https://shields.io/</a>
+          service. Preview the badges on this tool and then copy the
+          Markdown/HTML to your GitHub repo's <b>README.md</b> file. 
+          These metadata badges can help people learn
           about your repo at a glance and make it look professional.
         </p>
-
         <p>
           This tool adds <b>convenience</b> and <b>productivity</b> to your life
           so you can make badges quickly and easily and get on with the coding.
         </p>
-
         <p>
           The power here is in narrowing the choices and putting less burden on
           your to figure out rules and syntax or read long URLs. For badges that
@@ -49,7 +44,6 @@
           by hand (and do all the tedious things like escaping characters and
           learning the syntax for each badge.
         </p>
-
         <p>
           I use this tool regularly for myself - I hope you will too. If you
           want to contribute, issues and PRs are welcome.
